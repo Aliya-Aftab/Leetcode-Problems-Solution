3147685bb1ee9c343939a8f1282201020835d2a6
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2025-12-17 11:28:06 +0530

    Time: 0 ms (100%), Space: 22.4 MB (30.04%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+    int n=arr.size();
+    if(n<3){
+    return 0;
+    } 
+    int i=0;
+     int maxi=0;
+    while(i<n-1)
+    {
+    int upCnt=0;
+    int downCnt=0;
+    while(i<n-1 && arr[i]<arr[i+1]){
+    upCnt++;
+    i++;
+    }
+    while(i<n-1 && arr[i]>arr[i+1]){
+    downCnt++;
+    i++;
+    }
+    if(upCnt>0 && downCnt>0){
+    maxi=max(maxi, upCnt+downCnt+1);
+    }
+    if(upCnt == 0 && downCnt == 0){
+    i++;
+    }
+    }
+    return maxi;
+    }
+};
