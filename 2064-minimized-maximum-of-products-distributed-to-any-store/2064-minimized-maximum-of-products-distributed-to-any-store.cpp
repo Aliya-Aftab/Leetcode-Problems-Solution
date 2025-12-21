2f901074a93c6947b4ca6ac56d6d6288c952d0b4
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2025-12-21 15:55:08 +0530

    Time: 64 ms (19.19%), Space: 88.3 MB (98.06%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool distribution(vector<int>&quantities,int cap, int n){
+    long long stores=0;
+    for(int i=0;i<quantities.size();i++){
+    stores+=ceil((double)quantities[i]/(double)cap);
+    }
+    return stores<=n;
+    }
+    int minimizedMaximum(int n, vector<int>& quantities) {
+    int size=quantities.size();
+    if(n<size){
+    return -1;
+    } 
+    int maxi=INT_MIN;
+    for(int i=0;i<size;i++){
+    maxi=max(maxi, quantities[i]);
+    }
+    int lo=1;
+    int hi=maxi;
+    int ans=0;
+    while(lo<=hi){
+    int mid=lo+(hi-lo)/2;
+    if(distribution(quantities, mid, n)){
+    ans=mid;
+    hi=mid-1;
+    }
+    else{
+    lo=mid+1;
+    }
+    }
+    return ans;
+    }
+};
