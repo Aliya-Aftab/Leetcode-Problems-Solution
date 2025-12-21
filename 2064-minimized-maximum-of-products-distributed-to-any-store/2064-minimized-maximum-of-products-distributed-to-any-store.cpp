198864c50d076454adcaaf9269376eb3ce15fa7a
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2025-12-21 16:02:31 +0530

    Time: 58 ms (24.03%), Space: 88.3 MB (98.06%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -16,17 +16,15 @@ public:
     }
     int lo=1;
     int hi=maxi;
-    int ans=0;
     while(lo<=hi){
     int mid=lo+(hi-lo)/2;
     if(distribution(quantities, mid, n)){
-    ans=mid;
     hi=mid-1;
     }
     else{
     lo=mid+1;
     }
     }
-    return ans;
+    return lo;
     }
 };
