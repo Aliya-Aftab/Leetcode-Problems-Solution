dfe2ce1dfb862119ff3fff63b4288f52e6fa6c7a
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2025-12-28 13:54:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (36.59%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.cpp
+++ 0910-smallest-range-ii/0910-smallest-range-ii.cpp
@@ -7,6 +7,9 @@ public:
     int largest=nums[n-1]-k;
     int ans=nums[n-1]-nums[0];
     for(int i=0;i<n-1;i++){
+    // if (nums[i+1] - k < 0) {
+    // continue;
+    // }
     int mini=min(smallest, nums[i+1]-k);
     int maxi=max(largest, nums[i]+k);
     ans=min(ans, maxi-mini);
@@ -14,3 +17,6 @@ public:
     return ans; 
     }
 };
+
+// TC : O(nlogn)
+// SC : O(1)
