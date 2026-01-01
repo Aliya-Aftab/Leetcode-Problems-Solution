f9d335b3211662ca9317b2b8ea081d9c1ede72da
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-01-01 19:15:30 +0530

    Time: 1247 ms (5%), Space: 11.6 MB (83.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countExpressions(vector<int>&nums, int n, int idx,int currSum, int target){
+    int countExpressions(vector<int>&nums, int n, int idx,int &currSum, int target){
     // base case
     if(idx==n){
     if(currSum==target){
@@ -12,10 +12,18 @@ public:
 
     }
     // addition + subtraction
-    return countExpressions(nums,n,idx+1,currSum+nums[idx],target) + countExpressions(nums,n,idx+1,currSum-nums[idx], target);   
+    currSum+=nums[idx];
+    int add=countExpressions(nums,n,idx+1,currSum,target);
+    currSum-=nums[idx];
+    
+    currSum-=nums[idx];
+    int sub=countExpressions(nums,n,idx+1,currSum, target);  
+    currSum += nums[idx];
+    return add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
     int n=nums.size();
-    return countExpressions(nums,n, 0,0,target);
+    int currSum=0;
+    return countExpressions(nums,n, 0,currSum,target);
     }
 };
