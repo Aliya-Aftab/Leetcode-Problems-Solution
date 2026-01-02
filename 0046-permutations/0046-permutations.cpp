f1e565079b35cd3a5852fe39131c23e9158ff394
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-01-02 17:07:31 +0530

    Time: 0 ms (100%), Space: 10.5 MB (69.59%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    void generate(vector<int>&nums,int n, vector<int>&temp, vector<int>&visited, 
-     vector<vector<int>>&ans){
+    void generate(vector<int>&nums,int n, vector<vector<int>>&ans, int idx){
     // base case
-    if(temp.size()==nums.size()){
-    ans.push_back(temp);
+    if(idx==n){
+    ans.push_back(nums);
     return;
     }
-     for(int i=0;i<n;i++){
-     if(visited[i]){
-     continue;
-     }
-     temp.push_back(nums[i]);
-     visited[i]=1;
-     generate(nums,n,temp,visited,ans);
-     visited[i]=0;
-     temp.pop_back();
-     }
-     }
+    for(int i=idx;i<n;i++){
+    swap(nums[idx], nums[i]);
+    generate(nums,n,ans,idx+1);
+    swap(nums[idx], nums[i]);
+    }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
-    int n=nums.size();
-    vector<int>temp;
-    vector<int>visited(n,0);
     vector<vector<int>>ans;
-    generate(nums,n,temp,visited,ans);
+    int n=nums.size() ;
+    generate(nums,n,ans,0);
     return ans;
     }
 };
