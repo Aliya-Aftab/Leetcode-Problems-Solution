83bc36a86ff32223fc9afb8b601c7936aa48ade8
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-01-02 22:29:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (43.97%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -7,11 +7,11 @@ public:
     return '0';
     }
     int len=pow(2,n) - 1;
-    int mid=len/2;
-    if(k<=mid){
+    int mid=len/2  +1;
+    if(k<mid){
     return findKthBit(n-1,k);
     }
-    else if(k==mid+1){
+    else if(k==mid){
     return '1';
     }
     else{
