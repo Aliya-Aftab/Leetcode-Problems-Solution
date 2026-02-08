3595ebff71346666fc2f3acb0c88243bca8132b7
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-02-08 12:23:24 +0530

    Time: 31 ms (28.37%), Space: 89.5 MB (10.47%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -5,6 +5,7 @@ public:
     vector<int>nse(n,0);
     vector<int>pse(n,0);
     stack<int>st;
+    // next smaller element
     for(int i=0;i<n;i++){
     while(!st.empty() && heights[i]<heights[st.top()]){
     nse[st.top()]=i;
@@ -16,6 +17,7 @@ public:
     nse[st.top()]=n;
     st.pop();
     }
+    // prev smaller element
     for(int i=n-1;i>=0;i--){
     while(!st.empty() && heights[i]<heights[st.top()]){
     pse[st.top()]=i;
