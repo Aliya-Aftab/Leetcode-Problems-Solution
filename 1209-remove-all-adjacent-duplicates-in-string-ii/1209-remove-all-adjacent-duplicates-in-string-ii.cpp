6d80ed2451a139c8e4f3508ff38bd0ebe74a05ae
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-02-09 12:20:36 +0530

    Time: 7 ms (61.92%), Space: 15.6 MB (17.31%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -4,10 +4,7 @@ public:
     int n=s.length();
     stack<pair<char, int>>st;
     for(int i=0;i<n;i++){
-    // if(!st.empty() && st.top().second==k){
-    // st.pop();
-    // }
-     if( !st.empty() && st.top().first==s[i]){
+    if( !st.empty() && st.top().first==s[i]){
     st.top().second++;
     if(st.top().second==k){
     st.pop();
