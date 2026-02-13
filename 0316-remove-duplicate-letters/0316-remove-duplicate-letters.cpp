3b4d7d9668ba9be533b7fd72eed896d9a319b619
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-02-13 13:25:30 +0530

    Time: 0 ms (100%), Space: 9.2 MB (85.67%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-    string temp="";
-    vector<bool>visited(26,0);
-    vector<int>freq(26,0);
     int n=s.length();
+    vector<int>freq(26,0);
+    vector<bool>visited(26,0);
+    // count frequency
     for(int i=0;i<n;i++){
     freq[s[i]-'a']++;
     }
+    string temp="";
     for(int i=0;i<n;i++){
-freq[s[i]-'a']--;
-   if( visited[s[i]-'a']){
-   continue;
-   }
-   
-   while(!temp.empty() && temp.back()>s[i] && freq[temp.back()-'a']>0){
-   visited[temp.back()-'a']=0;
-   temp.pop_back();
-   }
-   temp.push_back(s[i]);
-   visited[s[i]-'a']=1;
+    freq[s[i]-'a']--;
+    if(visited[s[i]-'a']){
+    continue;
+    }
+    while(!temp.empty() && s[i]<temp.back() && freq[temp.back()-'a']>0){
+    visited[temp.back()-'a']=0;
+    temp.pop_back();
+    }
+    temp.push_back(s[i]);
+    visited[s[i]-'a']=1;
     } 
-    return temp;   
+    return temp;  
     }
 };
