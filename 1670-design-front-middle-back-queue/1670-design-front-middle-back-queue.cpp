f85949cc8b4784c313d73b1241b832efc1a799c4
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-02-18 13:39:12 +0530

    Time: 4 ms (69.23%), Space: 27.2 MB (56.26%) - LeetHub

--- 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.cpp
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.cpp
@@ -1,166 +1,93 @@
 class FrontMiddleBackQueue {
-    struct Node {
-        int val;
-        Node* next;
-        Node* prev;
-        Node(int x) : val(x), next(nullptr), prev(nullptr) {}
-    };
-
-    Node* head;
-    Node* tail;
-    Node* mid;
-    int size;
-
+deque<int> left, right;
+void balance(){
+// If Left is too big, move 1 to Right
+        if (left.size() > right.size()) {
+            right.push_front(left.back());
+            left.pop_back();
+        }
+        // If Right is too big (gap > 1), move 1 to Left
+        if (right.size() > left.size() + 1) {
+            left.push_back(right.front());
+            right.pop_front();
+        }
+}
 public:
     FrontMiddleBackQueue() {
-        head = tail = mid = nullptr;
-        size = 0;
+        
     }
-
+    
     void pushFront(int val) {
-        Node* newNode = new Node(val);
-        if (size == 0) {
-            head = tail = mid = newNode;
-            size++;
-            return;
-        }
-        
-        newNode->next = head;
-        head->prev = newNode;
-        head = newNode;
-        size++; // Update size to New Size
-
-        // Rule: Even size -> Mid shifts Left
-        if (size % 2 == 0) {
-            mid = mid->prev;
-        }
+    left.push_front(val);
+    balance();    
     }
-
+    
     void pushMiddle(int val) {
-        if (size == 0) {
-            pushFront(val);
-            return;
-        }
-
-        Node* newNode = new Node(val);
-        
-        // Logic: Insert based on OLD size (before increment)
-        // If Old Size is Odd (1,3): Insert BEFORE mid.
-        if (size % 2 == 1) {
-            newNode->next = mid;
-            newNode->prev = mid->prev;
-            if (mid->prev) mid->prev->next = newNode;
-            else head = newNode;
-            mid->prev = newNode;
-        } 
-        // If Old Size is Even (2,4): Insert AFTER mid.
-        else {
-            newNode->prev = mid;
-            newNode->next = mid->next;
-            if (mid->next) mid->next->prev = newNode;
-            mid->next = newNode;
-        }
-
-        size++; 
-        mid = newNode; // The new node always becomes the median
+    if(left.size()<right.size()){
+    left.push_back(val);
+    } 
+    else{
+    right.push_front(val);
+    }   
     }
-
+    
     void pushBack(int val) {
-        Node* newNode = new Node(val);
-        if (size == 0) {
-            head = tail = mid = newNode;
-            size++;
-            return;
-        }
-
-        tail->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
-        size++; // Update size to New Size
-
-        // Rule: Odd size -> Mid shifts Right
-        if (size % 2 == 1) {
-            mid = mid->next;
-        }
+    right.push_back(val);
+    balance();   
     }
-
+    
     int popFront() {
-        if (size == 0) return -1;
-
-        int val = head->val;
-        Node* temp = head;
-
-        if (size == 1) {
-            head = tail = mid = nullptr;
-            size--;
-            delete temp;
-            return val;
-        }
-
-        head = head->next;
-        head->prev = nullptr;
-        size--; // Update size to New Size
-
-        // Rule: If New Size is Odd, Mid shifts Right
-        // (Because we removed from the Left, shifting center Right)
-        if (size % 2 == 1) {
-            mid = mid->next;
-        }
-
-        delete temp;
-        return val;
+    if (left.empty() && right.empty()){
+    return -1;
+    }  
+    int val;
+    if(left.empty()){
+    val=right.front();
+    right.pop_front();
+    } 
+    else{
+    val=left.front();
+    left.pop_front();
     }
-
+    balance();
+    return val;
+    }
+    
     int popMiddle() {
-        if (size == 0) return -1;
-        if (size <= 2) return popFront(); // Size 1 & 2 handled safely by popFront
-
-        int val = mid->val;
-        Node* toDelete = mid;
-        
-        // Stitch neighbors (safe because size > 2 implies mid is not head/tail)
-        mid->prev->next = mid->next;
-        mid->next->prev = mid->prev;
-
-        size--; // Update size to New Size
-
-        // Rule: 
-        // If New Size is Even (was Odd 3->2): Mid shifts Left (Prev)
-        // If New Size is Odd (was Even 4->3): Mid shifts Right (Next)
-        if (size % 2 == 0) {
-            mid = mid->prev;
-        } else {
-            mid = mid->next;
-        }
-
-        delete toDelete;
-        return val;
+    if (left.empty() && right.empty()){
+    return -1;
     }
-
+    int val;
+    if(left.size()==right.size()){
+    val=left.back();
+    left.pop_back();
+    }
+    else{
+    val=right.front();
+    right.pop_front();
+    }
+    balance();    
+    return val;
+    }
+    
     int popBack() {
-        if (size == 0) return -1;
-
-        int val = tail->val;
-        Node* temp = tail;
-
-        if (size == 1) {
-            head = tail = mid = nullptr;
-            size--;
-            delete temp;
-            return val;
-        }
-
-        tail = tail->prev;
-        tail->next = nullptr;
-        size--; // Update size to New Size
-
-        // Rule: If New Size is Even, Mid shifts Left
-        // (Because we removed from the Right, shifting center Left)
-        if (size % 2 == 0) {
-            mid = mid->prev;
-        }
-
-        delete temp;
-        return val;
+    if (left.empty() && right.empty()){
+    return -1;
+    } 
+    int val=right.back();
+    right.pop_back();
+    balance();
+    return val;   
     }
 };
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
