2a630f589b72adabe37dbef385135cf76e6bff7b
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-02-21 17:36:46 +0530

    Time: 0 ms (100%), Space: 18.3 MB (35.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-    int n=nums.size();
-    int sum=0;
-    for(int num: nums){
-    sum+=num;
-    }
-    int reqSum=(sum+target)/2;
-    if(abs(target)>sum|| (sum+target)%2==1){
-    return 0;
-    }
-    vector<vector<int>>dp(n+1, vector<int>(sum+1,0));
-    dp[0][0]=1;
-    for(int i=1;i<=n;i++){
-    for(int j=0;j<=reqSum;j++){
-    if(nums[i-1]<=j){
-    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
-    }
-    else{
-    dp[i][j]=dp[i-1][j];
-    }
-    }
-    } 
-    return dp[n][reqSum];    
+        int n = nums.size();
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int reqSum = (sum + target) / 2;
+        if (abs(target) > sum || (sum + target) % 2 == 1) {
+            return 0;
+        }
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= reqSum; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][reqSum];
     }
 };
