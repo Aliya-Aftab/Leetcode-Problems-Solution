d89f8fc3f012752897db5e79530bc22c1338d14e
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-05-25 17:21:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.26%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>>&t){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(i<0 || i>=m || j<0 || j>=n){
-            return 0;
-        }
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-        int right=solve(i, j+1, m, n, t);
-        int down=solve(i+1, j, m, n, t);
-        return t[i][j]=right+down;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>t(m, vector<int>(n, -1));
-        return solve(0, 0, m, n, t);
+       vector<vector<int>>dp(m, vector<int>(n,0));
+       // dp[i][j]=no. of ways to reach at the cell i, j from 0,0
+       // can be stated as through how many ways we have reached at the curr cell
+       for(int j=0; j<n; j++){
+        dp[0][j]=1;
+       }
+       for(int i=0; i<m; i++){
+        dp[i][0]=1;
+       }
+       for(int i=1; i<m; i++){
+        for(int j=1; j<n; j++){
+            dp[i][j]=dp[i][j-1] + dp[i-1][j];
+        }
+       }
+       return dp[m-1][n-1];
     }
 };
