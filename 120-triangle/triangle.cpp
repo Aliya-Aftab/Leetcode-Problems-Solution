bb74e4dcd3a88853b2f14d9d3012b237fd116c60
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-05-26 18:41:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (78.29%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,16 +2,31 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>t=triangle;
+        vector<int>t=triangle[n-1];
 
         for(int row=n-2; row>=0; row--){
+            // row 0 has 1 element, row 1 has 2 element
+            // that's why iterating like col<=row.
             for(int col=0; col<=row; col++){
-                t[row][col]=t[row][col]+ min(t[row+1][col], t[row+1][col+1]);
+                t[col]=triangle[row][col]+ min(t[col], t[col+1]);
             }
         }
-        return t[0][0];
+        return t[0];
     }
 };
 /*
 dp[i][j] = min path sum from triangle[i][j] to bottom.
+    2
+   3 4
+  6 5 7
+ 4 1 8 3  <-- Bottom Row (n-1)
+
+    2
+   3 4
+  7 6 10  <-- This row now contains the "best cost to finish" from here
+
+     2
+    9 10
+
+    11
 */
