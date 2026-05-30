9f376a73deb979f4ecb17b4e287934c58ebea01d
Aliya-Aftab <166231905+Aliya-Aftab@users.noreply.github.com>
2026-05-30 14:20:33 +0530

    Time: 112 ms (32.70%) | Memory: 175.3 MB (13.69%) - LeetSync

---
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int n;
+    int child2(int i, int j, vector<vector<int>>& fruits, vector<vector<int>>&t2){
+        // handling boundary
+        if(i>=n || j<0 || j>=n || j<i){
+            return -1e9;
+        }
+        if(i == n-1 && j == n-1){
+            return 0;
+        }
+        if(t2[i][j]!=-1){
+            return t2[i][j];
+        }
+        int left=child2(i+1, j-1, fruits, t2);
+        int down=child2(i+1, j, fruits, t2);
+        int right=child2(i+1, j+1, fruits, t2);
+        return t2[i][j]=fruits[i][j] + max({left, down, right});
+    }
+    
+    int child3(int i, int j, vector<vector<int>>& fruits, vector<vector<int>>&t3){
+        // handling boundary
+        if(i>=n || i<0|| j<0 || j>=n || i<j){
+            return -1e9;
+        }
+        if(i == n-1 && j == n-1){
+            return 0;
+        }
+        if(t3[i][j]!=-1){
+            return t3[i][j];
+        }
+        int topRight=child3(i-1, j+1, fruits,t3);
+        int right=child3(i, j+1, fruits,t3);
+        int bottomRight=child3(i+1, j+1, fruits,t3);
+        return t3[i][j]= fruits[i][j] + max({topRight, right, bottomRight});
+    }
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        n=fruits.size();
+        vector<vector<int>>t2(n, vector<int>(n, -1));
+        vector<vector<int>>t3(n, vector<int>(n, -1));
+        int c1=0;
+        for(int i=0; i<n; i++){
+            c1+=fruits[i][i];
+            fruits[i][i]=0;
+        }
+        int c2=child2(0, n-1, fruits, t2);
+        int c3=child3(n-1, 0, fruits, t3);
+        return c1+c2+c3;
+    }
+};
+
+/*
+each child has to make exactly n-1 moves.
+child 1: if has to reach n-1, n-1 => only possible move is moving diagonally, otherwise it wouldn't be able to reach in n-1 steps.
+child 2: since, child 1 has picked up diagonal elements => 
+child 2 can't move in diagonals(i==j). Note, for reaching at
+n-1, n-1 cell,  child 2 has to take steps only in the one triangular phase. It cannot move where i>j.
+child 3: similar case as of child 2 => will not move to (i==j && j>i).
+*/
